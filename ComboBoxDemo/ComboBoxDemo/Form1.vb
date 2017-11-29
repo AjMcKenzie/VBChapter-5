7c916716f1fa51f51bfe4b81a3f17564e3d75c3f
Andrew <heldiggris7@gmail.com>
2017-11-29 10:36:40 -0600

    update ComboBoxDemo

---
+++ ComboBoxDemo/ComboBoxDemo/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        lblCountry.Text = cboCountries.Text
+        lblPlay.Text = cboPlays.Text
+        lblArtist.Text = cboArtists.Text
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        cboArtists.SelectedIndex = -1
+
+        cboCountries.SelectedIndex = -1
+        cboCountries.Text = String.Empty
+        cboPlays.SelectedIndex = -1
+        lblArtist.Text = String.Empty
+        lblCountry.Text = String.Empty
+        lblPlay.Text = String.Empty
+    End Sub
+End Class
