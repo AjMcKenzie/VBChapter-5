56384f211084fc1d438ad4422eca2ea2a7ad4476
Andrew <heldiggris7@gmail.com>
2017-12-07 10:43:47 -0600

    update DistanceCalc

--- CoinToss/CoinToss/DistanceCalculator.vb
+++ CoinToss/CoinToss/DistanceCalculator.vb
@@ -1,10 +1,23 @@
 ﻿Public Class DistanceCalculator
+    Dim Speed As Decimal
+    Dim Time As Decimal
+    Dim Distance As Decimal
+    Dim Hours As Decimal
     Sub Calc()
-        Dim Speed As Decimal
-        Dim Time As Decimal
-        Dim Distance As Decimal
-        Dim Hours As Decimal
+        Hours = Hours + 1
+        Distance = Speed * Hours
+        listDistance.Items.Add(Hours & "              " & Distance.ToString)
+    End Sub
 
+    Sub Clear()
+        listDistance.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
 
         Speed = InputBox("Enter the speed vehical in miles-per-hour")
         Time = InputBox("Enter the amount of time, in hours")
@@ -14,24 +27,9 @@
         listDistance.Items.Add("Hours      Distance Traveled")
         listDistance.Items.Add("------------------------------------------------------")
         Do While Hours < Time
-            Hours = Hours + 1
-            Distance = Speed * Hours
-            listDistance.Items.Add(Hours & "              " & Distance.ToString)
+            Calc()
         Loop
         listDistance.Items.Add("Total Distance: " & Distance.ToString)
-    End Sub
-    Sub Clear()
-        listDistance.Items.Clear()
-    End Sub
-
-
-    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-        Me.Close()
-    End Sub
-
-    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-        Calc()
-
     End Sub
 
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
